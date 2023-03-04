2a5b1ea71e2f7c2dcd556739312501336fa4ecb9
unknown <2051060410@e.tlu.edu.vn>
2023-03-04 11:25:36 +0700

    up

--- BTTH01/code/login.php
+++ BTTH01/code/login.php
@@ -34,22 +34,47 @@
         </nav>
 
     </header>
-   <body>
-        <div class="login-form">
-            <h2>Login</h2>
-            <form>
-                <label for="username">Username:</label>
-                <input type="text" id="username" name="username" placeholder="Enter your username">
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" placeholder="Enter your password">
-                <input type="submit" value="Login">
-            </form>
+    <main class="container mt-5 mb-5">
+        <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
+        <div class="d-flex justify-content-center h-100">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Sign In</h3>
+
+                    </div>
+                    <div class="card-body">
+                        <form>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="txtUser"><i class="fas fa-user"></i></span>
+                                <input type="text" class="form-control" placeholder="username" >
+                            </div>
+
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="txtPass"><i class="fas fa-key"></i></span>
+                                <input type="text" class="form-control" placeholder="password" >
+                            </div>
+
+                            <div class="row align-items-center remember">
+                                <input type="checkbox">Remember Me
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" value="Login" class="btn float-end login_btn">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="card-footer">
+                        <div class="d-flex justify-content-center ">
+                            Don't have an account?<a href="#" class="text-warning text-decoration-none">Sign Up</a>
+                        </div>
+                        <div class="d-flex justify-content-center">
+                            <a href="#" class="text-warning text-decoration-none">Forgot your password?</a>
+                        </div>
+                    </div>
+                </div>
+
         </div>
-
-
-    <footer class="bg-white d-flex justify-content-center align-items-center border-top border-secondary  border-2" style="height:80px">
-        <h4 class="text-center text-uppercase fw-bold">TLU's music garden</h4>
-    </footer>
+    </main>
+   
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
